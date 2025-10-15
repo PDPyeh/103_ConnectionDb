1348811ff2a9e69ca05783679558d72228a3bdc8
Pradipa Y <merakayamsstraat@gmail.com>
2025-10-15 09:53:31 +0700

    membuat html untuk website

--- app.js
+++ app.js
@@ -1,41 +1,34 @@
 const express = require('express');
 const mysql = require('mysql2');
+const path = require('path');
 const app = express();
 
 const PORT = 3000;
 
-// Konfigurasi koneksi ke MySQL
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'root',         
-  password: '',         
+  user: 'root',         // ubah sesuai MySQL kamu
+  password: '',         // ubah kalau pakai password
   database: 'mahasiswa'
 });
 
-// Cek koneksi database
 db.connect((err) => {
   if (err) {
     console.error('Koneksi database gagal:', err);
   } else {
     console.log('Berhasil konek ke database MySQL!');
-    
   }
 });
 
-// Endpoint GET untuk ambil semua data biodata
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/biodata', (req, res) => {
-  const sql = 'SELECT * FROM biodata';
-  db.query(sql, (err, results) => {
-    if (err) {
-      console.error('Error ambil data:', err);
-      res.status(500).json({ message: 'Gagal ambil data' });
-    } else {
-      res.json(results);
-    }
+  db.query('SELECT * FROM biodata', (err, results) => {
+    if (err) return res.status(500).json({ message: 'Gagal ambil data' });
+    res.json(results);
   });
 });
 
-// Jalankan server
 app.listen(PORT, () => {
   console.log(`Server jalan di http://localhost:${PORT}`);
 });
